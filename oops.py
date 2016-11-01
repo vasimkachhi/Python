67a7bd9f3cf429d8f75ecdf4ddf66f2d0f3572c1
vasimkachhi <vasimkachhi@gmail.com>
2016-11-01 11:33:17 +0530

    opps

--- oops.py
+++ oops.py
@@ -1,28 +1,35 @@
-class a:
+class a(object):
     def met(self):
         print 'a'
+    # pass
+
+
 class b(a):
+    # pass
     def met(self):
         print 'b'
+
+
 class c(a):
     # pass
     def met(self):
         print 'c'
 
-class d(c,b):
-    def met(self):
-        super(d,self).met()
-        print 'd'
+
+class d(c, b):
     # pass
+    def met(self):
+        # super(d, self).met()
+        print 'd'
 o = d()
 print type(o)
-# o.met()
-# super(b, o).met()
+o.met()
+# super(d, o).met()
 
 
 
-print 3/2
-print 3//2
+# print 3/2
+# print 3//2
 
-print 3/2.0
-print 3//2.0
+# print 3/2.0
+# print 3//2.0
