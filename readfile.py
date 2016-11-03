efe01f2b815e16e6f6b8b13e3a8b7f091fa157ba
vasimkachhi <vasim@123>
2016-11-04 00:36:05 +0530

    subfolders

--- readfile.py
+++ readfile.py
@@ -1,10 +1,15 @@
 import glob
 import os
-
-path = 'C:\Users\Vasim\Desktop\GITHUB'
-for filename in glob.glob(os.path.join(path, '*.py')):
+for filename in glob.glob('C:\Users\Vasim\Desktop\GITHUB\*py'):
     print filename
     dbname = os.path.splitext(os.path.basename(filename))[0]
     print dbname
     with open(filename) as fp:
         data = fp.read()
+
+# sub directories also
+import os, glob, itertools
+configfiles = itertools.chain.from_iterable(glob.iglob(os.path.join(root,'*.*'))
+                         for root, dirs, files in os.walk('C:\Users\Vasim\Desktop\GITHUB'))
+for a in enumerate(configfiles):
+    print a
